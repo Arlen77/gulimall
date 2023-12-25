4e7f89648e230a998394a4fdd82057f2f3ab914b
Arlen <2733262074@qq.com>
2023-12-25 20:35:29 +0800

    所有微服务项目基础代码的生成

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,25 @@
 package com.atguigu.gulimall.product;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+   1.整合Mybatis-plus
+     1.1）导入依赖
+     <dependency>
+         <groupId>com.baomidou</groupId>
+         <artifactId>mybatis-plus-boot-starter</artifactId>
+         <version>3.2.0</version>
+     </dependency>
+    1.2）配置
+        1.2.1）配置数据源；
+            导入数据库的驱动
+            在application.yml配置数据源相关信息
+        1.2.2）配置mybatis-plus：
+            使用@MapperScan告诉Mybatis-plus sql映射文件的位置
+ */
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
