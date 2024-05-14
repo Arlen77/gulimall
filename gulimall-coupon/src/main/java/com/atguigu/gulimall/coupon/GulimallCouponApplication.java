39ff2f78e18bb46553a4f4ad6466dbfc9da335b9
Arlen77 <114480246+Arlen77@users.noreply.github.com>
2024-05-14 16:04:59 +0900

    nacos，openfeign，gateway的结构开发

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -3,7 +3,23 @@ package com.atguigu.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/*
+* 如何使用nacos配置中心
+* 1，引入依赖
+* <dependency>
+	<groupId>com.alibaba.cloud</groupId>
+	<artifactId>spring-cloud-starter-alibaba-nacos-config</artifactId>
+ </dependency>
+ * 2,创建一个bootstrap.properties
+ * spring.application.name=gulimall-coupon
+   spring.cloud.nacos.config.server-addr=127.0.0.1:8848
+   3,nacos配置中心默认添加配置列表 gulimall-coupon.properties
+   4，动态获取配置 添加
+   @RefreshScope 动态获取刷新配置
+   @Value("${coupon.user.age}") 获取到配置
+* */
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.coupon.dao")
 @SpringBootApplication
 public class GulimallCouponApplication {
--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -2,7 +2,19 @@ package com.atguigu.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/*
+* Openfeign 远程调用
+* 1，pom文件里引入open-feign
+* 2，编写接口，告诉springboot这个接口需要调用远程服务
+*  2.1,声明接口的每一个方法调用哪个远程服务的哪个请求
+* 3，开启远程调用 @EnableFeignClients
+*
+* */
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
 
